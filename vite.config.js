685eaf33dbad612aabd528cfbd3f9075db1f3ad0
HazimZaghlol <business333man@gmail.com>
2025-01-07 21:32:07 +0200

    second

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/first-react-project/",
+  base: "/reactApp/",
   plugins: [react()],
 });
